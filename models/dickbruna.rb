08d2656ed59c521d9f5355c9c5614533eec5780b
kekoyana <kekoyana0@gmail.com>
2019-03-10 22:22:57 +0900

    一覧表示まで作成

--- models/dickbruna.rb
+++ models/dickbruna.rb
@@ -1,14 +1,42 @@
+# frozen_string_literal: true
+
 require 'rss'
+require 'models/article'
 
 # dickbruna class
 class Dickbruna
-  URL = 'https://www.dickbruna.jp/index.rdf'.freeze
+  URL = 'https://www.dickbruna.jp/index.rdf'
+  attr_accessor :articles
+
+  def initialize
+    build_articles
+  end
 
   def resource
     @resource ||= RSS::Parser.parse(URL, true)
   end
 
-  def items
-    resource.items.map(&:description)
+  private
+
+  def build_articles
+    @articles = resource.items.each_with_object([]) do |item, ary|
+      article = build_article_by(item)
+      next unless article
+
+      ary << article
+    end
+  end
+
+  def build_article_by(item)
+    matches = item.description.match(/期間：(.+)場所：(.+)/)
+    return unless matches
+
+    Article.new do |article|
+      article.period = matches[1]
+      article.place  = matches[2]
+      article.title  = item.title
+      article.body   = item.description
+      article.link   = item.link
+    end
   end
 end
